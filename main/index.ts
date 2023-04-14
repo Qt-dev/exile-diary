aa5c7972028c042ec5821f15d22ea1cc9d94a4f4
Quentin Devauchelle <quentin@devauchelle.eu>
2023-04-14 09:47:12 -0700

    feat: Make the app launchable in dev

---
+++ main/index.ts
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path';
+const devUrl = 'http://localhost:3000'
+enum SYSTEMS {
+  WINDOWS = 'win32',
+  LINUX = 'debian',
+  MACOS = 'darwin',
+}
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  require('electron-reload')(__dirname, {
+    electron: path.join(
+      __dirname,
+      '..',
+      '..',
+      'node_modules',
+      '.bin',
+      'electron' + (process.platform === SYSTEMS.WINDOWS ? '.cmd' : '')
+    ),
+    forceHardReset: true,
+    hardResetMethod: 'exit',
+  });
+
+  win.loadURL(devUrl);
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
