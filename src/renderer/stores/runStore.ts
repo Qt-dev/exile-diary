6f4c0095710d7c4d3a5695f999c74404d5cb84d2
Quentin Devauchelle <quentin@devauchelle.eu>
2023-04-21 19:45:24 -0700

    feat: Cleanup run store if the size gets too big

--- src/renderer/stores/runStore.ts
+++ src/renderer/stores/runStore.ts
@@ -7,6 +7,7 @@ export default class RunStore {
   runs : Run[] = [];
   isLoading = true;
   size = 10;
+  maxSize = 100; // This can be changed in the future
 
   constructor() {
       makeAutoObservable(this);
@@ -25,13 +26,16 @@ export default class RunStore {
     this.isLoading = true;
     electronService.ipcRenderer.invoke('load-runs', {size}).then((runs) => {
       runInAction(() => {
-        const ids = this.runs.map(r => r.runId);
+        const ids = this.runs.sort((first, second) => first.runId > second.runId ? -1 : 1).map(r => r.runId);
         ids.forEach((id, i) => {
           if(!runs.find((m) => m.id === id)) {
             this.runs.splice(i, 1);
           }
         });
         runs.forEach((json) => this.updateRunFromServer(json));
+        if(this.runs.length > this.maxSize) {
+          this.runs.splice(this.maxSize, this.runs.length - this.maxSize);
+        }
         this.isLoading = false;
       });
     });
