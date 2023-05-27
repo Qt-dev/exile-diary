c8a811597af0857d451dd3d4bd900a9cecb01f37
Quentin Devauchelle <quentin@devauchelle.eu>
2023-05-27 09:28:31 -0700

    feat: Add Rates Manager

---
+++ src/main/RatesManager.ts
@@ -0,0 +1,28 @@
+import DB from './db/rates';
+
+class RatesManager {
+  rates: {
+    [key: string] : {
+      [key: string] : any;
+    };
+  } = {};
+
+  constructor() {}
+
+  async fetchRatesForDay(league: string, date: string) : Promise<void> {
+    const rates = await DB.getFullRates(league, date);
+    this.rates[date] = this.rates[date] || {};
+    this.rates[date][league] = rates;
+  }
+
+  async getCurrencyValue(league: string, date: string, currency: string) : Promise<number> {
+    if (!this.rates[date] || !this.rates[date][league]) {
+      await this.fetchRatesForDay(league, date);
+    }
+    return this.rates[date][league]['Currency'][currency];
+  }
+}
+
+const ratesManager = new RatesManager();
+
+export default ratesManager;
---
+++ src/main/db/rates.ts
@@ -0,0 +1,27 @@
+import DB from './index';
+import logger from 'electron-log';
+import zlib from 'zlib';
+
+export default {
+  getFullRates: async (league: string, date: string) : Promise<any> => {
+    logger.info(`Getting rates for ${date} (league: ${league}) from DB`);
+    const query = 'SELECT date, data FROM fullrates WHERE date <= ? OR date = (SELECT min(date) FROM fullrates) ORDER BY date DESC';
+
+    try {
+      const [ { data } ] = await DB.all(query, [ date ], league) as any[];
+      return await new Promise((resolve, reject) => {
+        zlib.inflate(data, (err, buffer) => {
+          if (err) {
+            // old data - compression not implemented yet, just parse directly
+            resolve(JSON.parse(data));
+          } else {
+            resolve(JSON.parse(buffer.toString()));
+          }
+        });
+      });
+    } catch (err) {
+      logger.error(`Error getting rates for ${date} (league: ${league}): ${JSON.stringify(err)}`);
+      return {};
+    }
+  }
+}
