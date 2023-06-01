9ea7ba045f1cf7bdef91219d6e48cdde796cb8d8
Quentin Devauchelle <quentin@devauchelle.eu>
2023-06-01 08:47:45 -0700

    feat: Build basic frontend for Stash Tab Settings

--- src/renderer/components/StashSettings/StashSettings.tsx
+++ src/renderer/components/StashSettings/StashSettings.tsx
@@ -1,13 +1,107 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { Observer, observer } from 'mobx-react-lite';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import classNames from 'classnames';
+import './StashSettings.css';
+
 import { electronService } from '../../electron.service';
-import { observer } from 'mobx-react-lite';
+import ListItemButton from '@mui/material/ListItemButton';
+import Checkbox from '@mui/material/Checkbox';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Button from '@mui/material/Button';
 const { logger } = electronService;
 
+const ContainerComponent = ({ children, isFolder, disabled, callback }) => {
+  if(isFolder) {
+    return <div className="Stash-Settings__List-Item-Container">{children}</div>;
+  } else {
+    return <ListItemButton dense onClick={callback} disabled={disabled}>{children}</ListItemButton>;
+  }
+};
+
+const SubTitleForType = {
+  'Folder': "Folder",
+  'MapStash': "(Map Stash Tab)",
+  'UniqueStash': "(Unique Stash Tab)",
+}
+
+const StashTabRow = ({ stashTab, indentationLevel = 0 }) => {
+  const isFolder = stashTab.type === 'Folder';
+  const rowClasses = classNames({
+    'Stash-Settings__List-Item': true,
+    [`Stash-Settings__List-Item--${stashTab.type}`]: true,
+    [`Stash-Settings__List-Item--level-${indentationLevel}`]: true,
+  });
+  const Icon = require(`../../assets/img/tabicons/${stashTab.type}.png`);
+  const toggleEnabled = async (e) => {
+    const newCheckedValue = !stashTab.tracked;
+    await stashTab.setTracking(newCheckedValue);
+  };
+  const subtitle = SubTitleForType[stashTab.type] ?? null;
+
+  return (
+    <>
+      <ListItem
+        key={stashTab.id}
+        sx={{
+          backgroundColor: stashTab.metadata?.colour ? `#${stashTab.metadata.colour }` : 'initial',
+          height: '2.5em',
+          marginLeft: `${indentationLevel * 25}px`,
+          width: `calc(100% - ${indentationLevel * 25}px)`
+        }}
+        disablePadding
+        className={rowClasses}>
+        <ContainerComponent callback={toggleEnabled} isFolder={isFolder} disabled={stashTab.disabled}>
+          <ListItemIcon sx={{ minWidth: 0 }} className='Stash-Settings__Item-Icon'>
+            <img className="Stash-Settings__Item-Icon__Image" src={Icon} alt={stashTab.type} />
+            {!isFolder && <Checkbox
+              size="small"
+              edge="start"
+              checked={stashTab.tracked}
+              sx={{
+                // color: pink[800],
+                '&.Mui-checked': {
+                  color: '#fff',
+                },
+              }}
+              />}
+          </ListItemIcon>
+          
+          <ListItemText>
+            {stashTab.name} {subtitle && subtitle}
+          </ListItemText>
+        </ContainerComponent>
+      </ListItem>
+      {isFolder && stashTab.children.map((child: any) => (
+        <ObservedStashTabRow stashTab={child} key={child.id} indentationLevel={indentationLevel + 1}/>
+        ))}
+    </>
+  );
+};
+const ObservedStashTabRow = observer(StashTabRow);
+
 const StashSettings = ({ store, settings }) => {
-  const stashes = useLoaderData();
-  // logger.info(stashes);
-  return <div>StashSettings</div>;
+  const handleStoreRefreshCallback= () => {
+    store.fetchStashTabs();
+  };
+  return (
+    <div className='Stash-Settings'>
+      <div className="Stash-Settings__Header">
+        <div>
+          <div>Select the stash tabs you want to track, The app will then start pulling stats from these periodically</div>
+          <div>Unique and Maps stash tabs are disabled because such tabs take so much time that Exile Diary would not be able to be able to read these in time for proper stats aggregation.</div>
+        </div>
+        <Button variant="outlined" disabled={store.isLoading} onClick={handleStoreRefreshCallback}>Refresh Tabs</Button>
+      </div>
+      <List className='Stash-Settings__List'>
+        {
+          store.stashTabs.map((stashTab: any) => (<ObservedStashTabRow stashTab={stashTab} key={stashTab.id} />))
+        }
+      </List>
+    </div>
+  );
 }
 
 export default observer(StashSettings);
--- src/renderer/stores/domain/stashTab.ts
+++ src/renderer/stores/domain/stashTab.ts
@@ -12,7 +12,14 @@ export type StashTabData = {
     color?: string; // 6 digits hex color
   }
   items?: ItemData[];
+  children?: StashTabData[];
+  tracked: boolean;
 };
+
+const DisabledTypes = [
+  'MapStash',
+  'UniqueStash'
+];
 
 export class StashTab {
   id: string;
@@ -25,6 +32,9 @@ export class StashTab {
     color?: string; // 6 digits hex color
   }
   items?: ItemData[];
+  children?: StashTab[];
+  disabled: boolean;
+  tracked: boolean = false;
   store = null;
 
   constructor(store, stashTabData: StashTabData) {
@@ -39,6 +49,9 @@ export class StashTab {
     this.index = stashTabData.index;
     this.metadata = stashTabData.metadata;
     this.items = stashTabData.items;
+    this.children = stashTabData.children?.map(child => new StashTab(store, child));
+    this.disabled = DisabledTypes.includes(this.type);
+    this.tracked = !!stashTabData.tracked;
   }
 
   update(stashTabData: StashTabData) {
@@ -47,5 +60,14 @@ export class StashTab {
     this.index = stashTabData.index;
     this.metadata = stashTabData.metadata;
     this.items = stashTabData.items;
+    this.disabled = DisabledTypes.includes(this.type);
+    this.tracked = !!stashTabData.tracked;
+  }
+
+  setTracking(tracked: boolean) {
+    if(!this.disabled) {
+      this.tracked = tracked;
+      // SEND TO BACKEND AND WAIT FOR CONFIRMATION
+    }
   }
 }
