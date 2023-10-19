57bbad7ce8da1ca844e9bb98c0ddcc1a902052be
Quentin Devauchelle <quentin@devauchelle.eu>
2023-10-19 08:09:39 -0700

    fix: Add missing call for moment duration format

--- src/main/modules/Utils.js
+++ src/main/modules/Utils.js
@@ -7,6 +7,7 @@ const logger = require('electron-log');
 const moment = require('moment');
 const momentDurationFormatSetup = require('moment-duration-format-commonjs');
 const zlib = require('zlib');
+momentDurationFormatSetup(moment);
 
 const Utils = {
   getParams: (url) => {
