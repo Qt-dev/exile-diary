74b58da4abbe2025d98508b82077ef2b1a24daca
Quentin Devauchelle <quentin@devauchelle.eu>
2023-11-17 19:44:26 -0800

    feat: Add possibility for Price to not show chaos

--- src/renderer/components/Pricing/Price.tsx
+++ src/renderer/components/Pricing/Price.tsx
@@ -3,26 +3,32 @@ import ChaosIcon from './ChaosIcon';
 import DivineIcon from './DivineIcon';
 import './Pricing.css';
 
-const OptionalDivineValue = ({ value, divinePrice }) => {
+const OptionalDivineValue = ({ value, divinePrice, displayChaos }) => {
   const parsedDivinePrice = parseFloat(divinePrice);
   const parsedValue = parseFloat(value);
   if (parsedDivinePrice > 0 && parsedValue > 0.01 * parsedDivinePrice) {
     return (
       <>
-        ({(parsedValue / parsedDivinePrice).toFixed(2)}
-        <DivineIcon />)
+        {displayChaos && '('}{(parsedValue / parsedDivinePrice).toFixed(2)}
+        <DivineIcon />{displayChaos && ')'}
       </>
     );
   }
   return <></>;
 };
 
-const Price = ({ value, divinePrice = 0 }) => {
+const Price = ({ value, divinePrice = 0, displayChaos = true }) => {
+  const shouldDisplayChaos = displayChaos || value < divinePrice;
   return (
     <span className="Price">
-      {value}
-      <ChaosIcon />
-      <OptionalDivineValue value={value} divinePrice={divinePrice} />
+      { shouldDisplayChaos && 
+        <>
+        {value}
+        <ChaosIcon />
+        {' '}
+        </>
+      }
+      <OptionalDivineValue value={value} divinePrice={divinePrice} displayChaos={shouldDisplayChaos} />
     </span>
   );
 };
